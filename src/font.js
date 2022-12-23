25a3c654e372bfdad73920fbef2d54ad288e29a3
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2022-12-23 06:36:45 +0530

    Application fonts

---
+++ src/font.js
@@ -0,0 +1,62 @@
+import LocalFont from "@next/font/local";
+
+export const AltonaSans = LocalFont({
+  src: "./fonts/altona_sans/AltonaSans-Regular.ttf",
+});
+export const Barlow = LocalFont({
+  src: "./fonts/Barlow/Barlow-Regular.ttf",
+});
+export const Cabin = LocalFont({
+  src: "./fonts/Cabin/Cabin-VariableFont_wdth,wght.ttf",
+});
+export const Catamaran = LocalFont({
+  src: "./fonts/Catamaran/Catamaran-VariableFont_wght.ttf",
+});
+export const DancingScript = LocalFont({
+  src: "./fonts/Dancing_Script/DancingScript-VariableFont_wght.ttf",
+});
+export const Fredoka = LocalFont({
+  src: "./fonts/Fredoka/Fredoka-VariableFont_wdth,wght.ttf",
+});
+export const Hubbali = LocalFont({
+  src: "./fonts/Hubballi/Hubballi-Regular.ttf",
+});
+export const Lato = LocalFont({ src: "./fonts/Lato/Lato-Regular.ttf" });
+export const LeagueSpartan = LocalFont({
+  src: "./fonts/League_Spartan/LeagueSpartan-VariableFont_wght.ttf",
+});
+export const Lora = LocalFont({
+  src: "./fonts/Lora/Lora-VariableFont_wght.ttf",
+});
+export const MaterialIcons = LocalFont({
+  src: "./fonts/Material Icons/MaterialIcons-Regular.ttf",
+});
+export const Montserrat = LocalFont({
+  src: "./fonts/Montserrat/Montserrat-VariableFont_wght.ttf",
+});
+export const Nunito = LocalFont({
+  src: "./fonts/Nunito/Nunito-VariableFont_wght.ttf",
+});
+export const OpenSans = LocalFont({
+  src: "./fonts/Open_Sans/OpenSans-VariableFont_wdth,wght.ttf",
+});
+export const Oxygen = LocalFont({ src: "./fonts/Oxygen/Oxygen-Regular.ttf" });
+export const Poppins = LocalFont({
+  src: "./fonts/Poppins/Poppins-Regular.ttf",
+});
+export const Quicksand = LocalFont({
+  src: "./fonts/Quicksand/Quicksand-VariableFont_wght.ttf",
+});
+export const Raleway = LocalFont({
+  src: "./fonts/Raleway/Raleway-VariableFont_wght.ttf",
+});
+export const Roboto = LocalFont({ src: "./fonts/Roboto/Roboto-Regular.ttf" });
+export const Rubik = LocalFont({
+  src: "./fonts/Rubik/Rubik-VariableFont_wght.ttf",
+});
+export const Sarabun = LocalFont({
+  src: "./fonts/Sarabun/Sarabun-Regular.ttf",
+});
+export const SourceSansPro = LocalFont({
+  src: "./fonts/Source_Sans_Pro/SourceSansPro-Regular.ttf",
+});
