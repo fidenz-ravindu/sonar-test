60d13fc5891f40d2c9104c858710fec6bb97ba21
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2022-12-25 06:29:12 +0530

    archievements route

---
+++ pages/archievements.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import { Component } from "react";
+
+export default class Archievements extends Component {
+    render() {
+        return (
+            <>
+                <Head>
+                    <title>Portfolio - Archievements</title>
+                </Head>
+            </>
+        );
+    }
+}
