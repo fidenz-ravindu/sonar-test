4954c91e30c50bf565bc457f393f4b9ff8f2cac0
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2022-12-25 06:29:52 +0530

    work route

---
+++ pages/work.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import { Component } from "react";
+
+export default class Work extends Component {
+  render() {
+    return (
+      <>
+        <Head>
+          <title>Portfolio - Work</title>
+        </Head>
+      </>
+    );
+  }
+}
