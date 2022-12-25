98d548348f798698984ba81e50f78cce15d279ba
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2022-12-25 06:27:36 +0530

    projects route

---
+++ pages/projects/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import { Component } from "react";
+
+export default class Projects extends Component {
+  render() {
+    return (
+      <>
+        <Head>
+          <title>Portfolio - Projects</title>
+        </Head>
+      </>
+    );
+  }
+}
