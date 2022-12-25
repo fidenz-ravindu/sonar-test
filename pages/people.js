5d57247a7a14b6ab9d2c482abca8740c9b7c2c38
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2022-12-25 06:29:39 +0530

    people route

---
+++ pages/people.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import { Component } from "react";
+
+export default class People extends Component {
+  render() {
+    return (
+      <>
+        <Head>
+          <title>Portfolio - People</title>
+        </Head>
+      </>
+    );
+  }
+}
