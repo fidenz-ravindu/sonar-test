4562c01d611678f35283336f803602dc08a5fb37
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2022-12-25 06:26:07 +0530

    BE get use info

---
+++ pages/api/details.js
@@ -0,0 +1,14 @@
+import { getConnection } from "../../src/mongo";
+import { getDetails } from "../../src/repos/user_repository";
+
+export default async function handler(req, res) {
+  if (req.method /*=== "POST"*/) {
+    await getConnection();
+
+    let response = await getDetails(req.query.fields);
+
+    res.status(200).json({ details: response });
+    return;
+  }
+  res.status(200).json({});
+}
