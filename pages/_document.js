3c362045031596bd0cf2b552e8473737c76ea5e6
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2022-12-25 06:28:43 +0530

    document structure

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Android_O_Preview_Logo.png/1024px-Android_O_Preview_Logo.png" />
+      </Head>
+      <body style={{ height: "100vh", overflow: "auto" }}>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
