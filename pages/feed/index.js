0a30c38642474284a0826a985fcd6fc6b451ca5d
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2022-12-25 06:27:00 +0530

    feed route

---
+++ pages/feed/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import { Component } from "react";
+
+export default class Feed extends Component {
+  render() {
+    return (
+      <>
+        <Head>
+          <title>Portfolio - Feed</title>
+        </Head>
+      </>
+    );
+  }
+}
