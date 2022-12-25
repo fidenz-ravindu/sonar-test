55306d467f47de50b70e16afb43aaa58690e2634
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2022-12-25 06:34:20 +0530

    onRoute effects

---
+++ src/components/RouteProgress.jsx
@@ -0,0 +1,12 @@
+import ComponentStyles from "../../styles/Component.module.css"
+
+export default function RouteProgress(props) {
+    return <span className={ComponentStyles.absolute} style={{
+        height: "3px",
+        backgroundColor: "lightcyan",
+        boxShadow: "0 0 6px var(--dark)",
+        zIndex: "50000",
+        top: "0",
+        ...props.style
+    }}></span>
+}
