609c5ee528ae257243b33a91dd43b947be6e7d43
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2022-12-25 06:37:48 +0530

    default mongodb connection

---
+++ src/mongo.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+let connection = undefined;
+
+const getConnection = async () => {
+  if (connection) return connection;
+
+  if (!process.env.MONGODB_URL) throw "No datasource found.";
+
+  connection = await mongoose.connect(process.env.MONGODB_URL).catch((err) => {
+    console.error(err);
+  });
+
+  return connection;
+};
+
+export { getConnection };
