7e335f845c45a41e23508632f9e652fbda5a0055
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2022-12-27 08:14:24 +0530

    api method changed

--- pages/api/work.js
+++ pages/api/work.js
@@ -2,9 +2,14 @@ import { getConnection } from "../../src/mongo";
 import { getWorkDetails } from "../../src/repos/work_repository";
 
 export default async function handler(req, res) {
-  await getConnection();
+  if (req.method === "POST") {
+    await getConnection();
 
-  res.status(200).json({
-    work: await getWorkDetails(),
-  });
+    res.status(200).json({
+      work: await getWorkDetails(),
+    });
+    return;
+  }
+
+  res.status(200).json({});
 }
