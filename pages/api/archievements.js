76de355a4a74761df3d708932e427b783a1c0fff
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2023-01-05 22:27:50 +0530

    connection caching

--- pages/api/archievements.js
+++ pages/api/archievements.js
@@ -1,7 +1,10 @@
+import { getConnection } from "../../src/mongo";
 import { getArchievements } from "../../src/repos/archievement_repository";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
+    await getConnection();
+
     res.status(200).json({ archievements: await getArchievements() });
     return;
   }
--- pages/api/gallery.js
+++ pages/api/gallery.js
@@ -1,7 +1,10 @@
+import { getConnection } from "../../src/mongo";
 import { getMedia } from "../../src/repos/gallery_repository";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
+    await getConnection();
+
     res.status(200).json({ gallery: await getMedia() });
     return;
   }
