248cbad88bbaf2d1f901753a39ae80818e43e316
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2023-01-05 11:27:59 +0530

    bugs fixed

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,7 +2,15 @@ import { Component } from "react";
 
 import ComponentStyles from "../../styles/Component.module.css";
 import Styles from "../../styles/Post.module.css";
-import { AltonaSans, Barlow, Hubbali, Nunito, OpenSans, Poppins, SourceSansPro } from "../font";
+import {
+  AltonaSans,
+  Barlow,
+  Hubbali,
+  Nunito,
+  OpenSans,
+  Poppins,
+  SourceSansPro,
+} from "../font";
 
 export default class Post extends Component {
   constructor(props) {
@@ -28,58 +36,144 @@ export default class Post extends Component {
     const { type, style } = this.props;
     switch (type) {
       case 1:
-        return <>
-          <div className={[Styles.postContainer, ComponentStyles.blink].join(" ")} style={style}>
-            <div className={[ComponentStyles.row, ComponentStyles.center, ComponentStyles.spaceBet, ComponentStyles.alignStart].join(" ")}>
-              <div >
-                <div className={Styles.container} style={{ padding: "15px 40px" }}></div>
+        return (
+          <>
+            <div
+              className={[Styles.postContainer, ComponentStyles.blink].join(
+                " "
+              )}
+              style={style}
+            >
+              <div
+                className={[
+                  ComponentStyles.row,
+                  ComponentStyles.center,
+                  ComponentStyles.spaceBet,
+                  ComponentStyles.alignStart,
+                ].join(" ")}
+              >
                 <div>
-                  <span className={[Styles.container, Styles.tag].join(" ")} style={{ padding: "8px 20px" }}></span>
-                  <span className={[Styles.container, Styles.tag].join(" ")} style={{ padding: "8px 20px" }}></span>
-                  <span className={[Styles.container, Styles.tag].join(" ")} style={{ padding: "8px 20px" }}></span>
-                  <span className={[Styles.container, Styles.tag].join(" ")} style={{ padding: "8px 20px" }}></span>
-                  <span className={[Styles.container, Styles.tag].join(" ")} style={{ padding: "8px 20px" }}></span>
+                  <div
+                    className={Styles.container}
+                    style={{ padding: "15px 40px" }}
+                  ></div>
+                  <div>
+                    <span
+                      className={[Styles.container, Styles.tag].join(" ")}
+                      style={{ padding: "8px 20px" }}
+                    ></span>
+                    <span
+                      className={[Styles.container, Styles.tag].join(" ")}
+                      style={{ padding: "8px 20px" }}
+                    ></span>
+                    <span
+                      className={[Styles.container, Styles.tag].join(" ")}
+                      style={{ padding: "8px 20px" }}
+                    ></span>
+                    <span
+                      className={[Styles.container, Styles.tag].join(" ")}
+                      style={{ padding: "8px 20px" }}
+                    ></span>
+                    <span
+                      className={[Styles.container, Styles.tag].join(" ")}
+                      style={{ padding: "8px 20px" }}
+                    ></span>
+                  </div>
                 </div>
+                <div
+                  className={Styles.container}
+                  style={{ padding: "8px 20px" }}
+                ></div>
               </div>
-              <div className={Styles.container} style={{ padding: "8px 20px" }}></div>
+              <div
+                className={Styles.container}
+                style={{ padding: "20px 5px" }}
+              ></div>
             </div>
-            <div className={Styles.container} style={{ padding: "20px 5px" }}></div>
-          </div>
-        </>;
+          </>
+        );
       case 2:
         const { post } = this.props;
 
-        let time, now = new Date(), posted = new Date(post.posted);
-        if ((now.getTime() - (1000 * 60 * 60 * 24)) < posted.getTime()) {
-          time = `${posted.getHours()}:${posted.getMinutes()}`
-          console.log(time)
+        let time,
+          now = new Date(),
+          posted = new Date(post.posted);
+        if (now.getTime() - 1000 * 60 * 60 * 24 < posted.getTime()) {
+          time = `${posted.getHours()}:${posted.getMinutes()}`;
+          console.log(time);
         } else if (now.getFullYear() === posted.getFullYear()) {
           time = `${this.months[posted.getMonth()]} ${posted.getDate()}`;
         } else {
           time = `${posted.getFullYear()} ${this.months[posted.getMonth()]}`;
         }
 
-        return <>
-          <div className={[Styles.postContainer, ComponentStyles.fadeIn].join(" ")} style={style}>
-            <div className={[ComponentStyles.row, ComponentStyles.center, ComponentStyles.spaceBet, ComponentStyles.alignStart].join(" ")}>
-              <div >
-                <div className={[Styles.postTitle, Nunito.className].join(" ")}> {post.title}</div>
+        return (
+          <>
+            <div
+              className={[Styles.postContainer, ComponentStyles.fadeIn].join(
+                " "
+              )}
+              style={style}
+            >
+              <div
+                className={[
+                  ComponentStyles.row,
+                  ComponentStyles.center,
+                  ComponentStyles.spaceBet,
+                  ComponentStyles.alignStart,
+                ].join(" ")}
+              >
                 <div>
-                  {post.tags.map(elm => <span key={Math.random()} className={[Styles.container, SourceSansPro.className, Styles.tag].join(" ")} >{elm}</span>)}
+                  <div
+                    className={[Styles.postTitle, Nunito.className].join(" ")}
+                  >
+                    {" "}
+                    {post.title}
+                  </div>
+                  <div>
+                    {post.tags.map((elm) => (
+                      <span
+                        key={Math.random()}
+                        className={[
+                          Styles.container,
+                          SourceSansPro.className,
+                          Styles.tag,
+                        ].join(" ")}
+                      >
+                        {elm}
+                      </span>
+                    ))}
+                  </div>
+                </div>
+                <div
+                  className={[
+                    Styles.postDate,
+                    Styles.date,
+                    OpenSans.className,
+                  ].join(" ")}
+                >
+                  {time}
                 </div>
               </div>
-              <div className={[Styles.postDate, Styles.date, OpenSans.className].join(" ")} >{time}</div>
+              {post.type === "Image" && post.link && (
+                <div className={[Styles.content, Poppins.className].join(" ")}>
+                  <div style={{ marginBottom: "15px", padding: "10px" }}>
+                    {post.content}
+                  </div>
+                  <div>
+                    {
+                      <img
+                        src={post.link}
+                        alt={"Post"}
+                        style={{ width: "100%" }}
+                      />
+                    }
+                  </div>
+                </div>
+              )}
             </div>
-            <div className={[Styles.content, Poppins.className].join(" ")} >
-              <div style={{ marginBottom: "15px", padding: "10px" }}>{post.content}</div>
-              <div>
-                {
-                  <img src={post.link} alt={"Post"} style={{ width: "100%" }} />
-                }
-              </div>
-            </div>
-          </div>
-        </>;
+          </>
+        );
     }
   }
 }
