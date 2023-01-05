a3fc04fc38c350d0f4fc6401df81f3f8b942dfa4
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2023-01-05 22:22:46 +0530

    archievement api

---
+++ pages/api/archievements.js
@@ -0,0 +1,10 @@
+import { getArchievements } from "../../src/repos/archievement_repository";
+
+export default async function handle(req, res) {
+  if (req.method === "POST") {
+    res.status(200).json({ archievements: await getArchievements() });
+    return;
+  }
+
+  res.status(200).json({});
+}
