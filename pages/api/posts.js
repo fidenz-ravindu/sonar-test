cec9b07657bf8dd652cb3ec1515f64a812f6763c
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2023-01-06 08:27:11 +0530

    bugs fixed

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,8 +1,10 @@
+import { getConnection } from "../../src/mongo";
 import { getPosts } from "../../src/repos/post_repository";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const { limit, page, filter } = req.query;
+    await getConnection();
 
     res.status(200).json({
       posts: await getPosts(limit, page, filter),
