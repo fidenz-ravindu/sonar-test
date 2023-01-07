d46b9aee030750d1ed6d627b8fc947bc048bccae
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2023-01-08 00:30:34 +0530

    gallery route and components

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -1,13 +1,146 @@
 import Head from "next/head";
-import { Component } from "react";
+import { Component, createRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Styles from "../../styles/Gallery.module.css";
+import ProjectStyles from "../../styles/Project.module.css";
+import ComponentStyles from "../../styles/Component.module.css";
+import { set } from "../../src/store";
+import { get_gallery_media } from "../../src/api/request";
+import { Atma } from "../../src/font";
+import Media from "../../src/components/Media";
 
-export default class Gallery extends Component {
+export default function Gallery(props) {
+  return (
+    <GalleryComponent
+      {...props}
+      dispatch={useDispatch()}
+      media={useSelector((state) => state.gallery)}
+      isWaiting={useSelector((state) => state.waiting)}
+    />
+  );
+}
+
+class GalleryComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      active: 0,
+      left: 0,
+      offset: 0
+    };
+
+    this.setActiveMedia = this.setActiveMedia.bind(this);
+    this.scrollTo = this.scrollTo.bind(this);
+    this.scroll = this.scroll.bind(this);
+    this.swipe = this.swipe.bind(this);
+
+    this.mediaContainer = createRef();
+  }
+
+  async getMedia() {
+    const { dispatch } = this.props;
+
+    dispatch(set({ key: "waiting", value: true }));
+    await get_gallery_media((res) => {
+      let i = 0;
+      dispatch(
+        set({
+          key: "gallery",
+          value: res.gallery.map((elm) => {
+            elm["key"] = i++;
+            return elm;
+          }),
+        })
+      );
+    });
+    dispatch(set({ key: "waiting", value: false }));
+  }
+
+  componentDidMount() {
+    this.getMedia();
+  }
+
+  setActiveMedia(key) {
+    this.setState({ active: key });
+  }
+
+  scrollTo(e) {
+    if (e)
+      this.scroll(-e.offsetLeft + (this.mediaContainer.current.clientWidth - e.clientWidth) / 2);
+  }
+
+  scroll(len) {
+    this.setState({
+      left: len
+    })
+  }
+
+  swipe(diff) {
+    this.setState({
+      offset: diff
+    })
+  }
+
   render() {
+    const { isWaiting, media } = this.props,
+      { active, left, offset } = this.state;
+
     return (
       <>
         <Head>
           <title>Portfolio - Gallery</title>
         </Head>
+
+        <main
+          className={[
+            ComponentStyles.center,
+            ComponentStyles.page,
+            ComponentStyles.center,
+          ].join(" ")}
+        >
+          {media.length === 0 && isWaiting && (
+            <div
+              className={[
+                ProjectStyles.loading,
+                Styles.mediaContainer,
+                ComponentStyles.center,
+              ].join(" ")}
+            ></div>
+          )}
+          {media.length > 0 && (
+            <div ref={this.mediaContainer} className={Styles.mediaContainer}>
+              <div className={Styles.content} style={{ left: `${left + offset}px` }} >
+                {media.map((elm) => (
+                  <Media
+                    rearrange={elm.key === 0}
+                    active={active}
+                    id={elm.key}
+                    len={media.length}
+                    setActive={(key) => {
+                      this.setActiveMedia(key);
+                    }}
+                    scroll={this.scrollTo}
+                    scrollLen={this.scroll}
+                    swipe={this.swipe}
+                    key={elm.key}
+                    post={elm}
+                  />
+                ))}
+              </div>
+            </div>
+          )}
+          {!isWaiting && media.length === 0 && (
+            <>
+              <div
+                className={[Atma.className, ComponentStyles.fadeIn].join(" ")}
+                style={{ margin: "25px", color: "wheat", fontSize: "1.2rem" }}
+              >
+                No results found.
+              </div>
+            </>
+          )}
+        </main>
       </>
     );
   }
---
+++ src/components/Media.jsx
@@ -0,0 +1,118 @@
+import { Component } from "react";
+import Styles from "../../styles/Gallery.module.css";
+import ComponentStyles from "../../styles/Component.module.css";
+import { Cabin, Catamaran, Hubbali, MaterialIcons, Montserrat, Nunito, Oxygen, Raleway, Roboto, Rubik } from "../font";
+
+export default class Media extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            x: -1,
+            shouldFocus: true
+        }
+
+        this.months = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+        ];
+
+        this.swipeEnd = this.swipeEnd.bind(this);
+    }
+
+    swipeEnd(e) {
+        const { x } = this.state, { scrollLen, setActive, swipe, id, len, scroll } = this.props;
+
+        if (x > 0) {
+            if (x === e.pageX) {
+                scroll(e.target.parentNode.parentNode);
+                setActive(id);
+            } else
+                if (Math.abs(x - e.pageX) >= e.target.clientWidth / 2) {
+                    if (id < len - 1 && x > e.pageX) {
+                        scroll(e.target.parentNode.parentNode.parentNode.children[id + 1]);
+                        setActive(id + 1);
+                    }
+                    else if (id > 0) {
+                        scroll(e.target.parentNode.parentNode.parentNode.children[id - 1]);
+                        setActive(id - 1);
+                    }
+                }
+            this.setState({ x: -1 })
+            swipe(0);
+        }
+    }
+
+    swipeMove(e) {
+        const { id, active, swipe } = this.props, { x } = this.state;
+        if (id === active && x >= 0) {
+            swipe(e.pageX - x);
+        }
+    }
+
+    render() {
+        const { className, style, post, active, swipe, id, scroll, rearrange } = this.props,
+            posted = new Date(post.uploaded),
+            activeElement = id === active,
+            { x } = this.state;
+
+        return (
+            <div onLoad={(e) => {
+                scroll(e.target.parentNode.parentNode.parentNode.children[0]);
+            }}
+                onMouseDown={(e) => {
+                    this.setState({ x: e.screenX })
+                }}
+                onTouchStart={(e) => {
+                    this.setState({ x: e.screenX })
+                }}
+                onMouseMove={(e) => {
+                    this.swipeMove(e);
+                }}
+                onTouchMove={(e) => {
+                    this.swipeMove(e);
+                }}
+                onMouseOut={this.swipeEnd}
+                onMouseUp={this.swipeEnd}
+                onTouchEnd={this.swipeEnd}
+                onTouchCancel={this.swipeEnd}
+                className={[Styles.media, ComponentStyles.center, ComponentStyles.relative, className].join(" ")}
+                style={{ scale: (activeElement ? '1' : '0.8'), filter: (activeElement ? "none" : "blur(4px)"), ...style }}>
+                <div className={[Styles.mediaContentContainer, ComponentStyles.center].join(" ")} onLoad={(e) => {
+                    const containers = e.target.parentNode.parentNode.parentNode.children;
+                    for (let i = 0; i < containers.length; i++) {
+                        const children = containers[i].children[0].children;
+                        if (children[0].clientWidth < children[0].clientHeight) {
+                            children[0].style.height = '100%';
+                        }
+
+                        children[1].style.width = `${children[0].clientWidth}px`;
+                        children[1].style.height = `${children[0].clientHeight}px`;
+
+                        e.target.parentNode.parentNode.style.opacity = '1';
+                    }
+                }}
+                >
+                    {post.type === "Image" && <img draggable={false} className={Styles.mediaContent} src={post.link} alt="Image of post" />}
+                    <div className={[Styles.metaData, ComponentStyles.absolute].join(" ")}>
+                        <div className={[Styles.mediaData, ComponentStyles.absolute].join(" ")}>
+                            <div className={[Styles.caption, Roboto.className].join(" ")}>{post.caption}</div>
+                            <div className={[ComponentStyles.center, Nunito.className].join(" ")} style={{ width: "fit-content", fontSize: "0.8rem" }}><span className={MaterialIcons.className}>location_on</span>{post.location}</div>
+                        </div>
+                        <div className={[ComponentStyles.absolute, Cabin.className].join(" ")} style={{ top: "5px", right: "5px", color: "wheat", fontSize: "0.7rem" }}>{`${posted.getFullYear()} ${this.months[posted.getMonth()]} ${posted.getDate()}`}</div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
