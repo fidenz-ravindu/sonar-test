21ba30f2bf05d58423eaef0affefa2f9ba691d46
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2023-01-14 17:45:16 +0530

    new features

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -150,6 +150,16 @@ class NavbarComponent extends Component {
           </div>
 
           <div className={Styles.navButtonContainer}>
+            <a href={"/CV.pdf"} className={[
+              ComponentStyles.btn,
+              ComponentStyles.center,
+              Nunito.className,
+              Styles.navButton
+            ].join(" ")}>
+              <svg className={Styles.buttonSVG} viewBox="0 0 48 48" fill="white">
+                <path d="M11 40q-1.2 0-2.1-.9Q8 38.2 8 37v-7.15h3V37h26v-7.15h3V37q0 1.2-.9 2.1-.9.9-2.1.9Zm13-7.65-9.65-9.65 2.15-2.15 6 6V8h3v18.55l6-6 2.15 2.15Z" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+              </svg>
+            </a>
             <Link
               href={"/contact"}
               className={[
