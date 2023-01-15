070b14780575c8905671da34c283bbe409ba123d
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2023-01-15 20:14:04 +0530

    google site verification

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Android_O_Preview_Logo.png/1024px-Android_O_Preview_Logo.png" />
+        <link
+          rel="icon"
+          href="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Android_O_Preview_Logo.png/1024px-Android_O_Preview_Logo.png"
+        />
+        <meta
+          name="google-site-verification"
+          content="NEHCvnQNYkKkAc5KGrDzxL8Djd867HczPDBN9UUp3io"
+        />
       </Head>
       <body style={{ height: "100vh", overflow: "auto" }}>
         <Main />
