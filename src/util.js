82b147cead634f8a322e3c49238e39c7ed5408e7
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2023-01-16 18:50:23 +0530

    removed duplicates

--- src/util.js
+++ src/util.js
@@ -30,3 +30,19 @@ export const months_shortened = [
   "Nov",
   "Dec",
 ];
+
+export const date_extract = (date) => {
+  let time,
+    now = new Date(),
+    posted = new Date(date);
+  if (now.getTime() - 1000 * 60 * 60 * 24 < posted.getTime()) {
+    time = `${posted.getHours()}:${posted.getMinutes()}`;
+    now.setHours(0, 0, 0, 0);
+    if (now.getTime() > posted.getTime()) time = "Yesterday";
+  } else if (now.getFullYear() === posted.getFullYear()) {
+    time = `${months_shortened[posted.getMonth()]} ${posted.getDate()}`;
+  } else {
+    time = `${posted.getFullYear()} ${months[posted.getMonth()]}`;
+  }
+  return time;
+};
