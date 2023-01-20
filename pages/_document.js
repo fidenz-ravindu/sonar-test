3e6832ebe0b89c105c43ffb4fb6675f3433b49c3
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2023-01-21 00:40:43 +0530

    gtag

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,19 @@
+import DOMParserReact from "dom-parser-react";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <DOMParserReact
+          source="<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WDMQGFW');</script>
+<!-- End Google Tag Manager -->"
+        />
         <link
           rel="icon"
           href="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Android_O_Preview_Logo.png/1024px-Android_O_Preview_Logo.png"
@@ -12,10 +22,21 @@ export default function Document() {
           name="google-site-verification"
           content="NEHCvnQNYkKkAc5KGrDzxL8Djd867HczPDBN9UUp3io"
         />
-        <meta name="author" content="Ravindu Denuwan" /> 
-        <meta name="keywords" content="developer, ravindu denuwan, michael, revington, revington dev's, revington developments, programmer, c++, java, html, css, js, javascript, web-development, front-end development, back-end development, software"/>
+        <meta name="author" content="Ravindu Denuwan" />
+        <meta
+          name="keywords"
+          content="developer, ravindu denuwan, michael, revington, revington dev's, revington developments, programmer, c++, java, html, css, js, javascript, web-development, front-end development, back-end development, software"
+        />
       </Head>
       <body style={{ height: "100vh", overflow: "auto" }}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WDMQGFW"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>
