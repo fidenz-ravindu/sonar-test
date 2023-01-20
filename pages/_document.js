3027595de912f129bf3c31dc71ea2c6dbebf8790
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2023-01-21 01:08:25 +0530

    bugs fix

--- pages/_document.js
+++ pages/_document.js
@@ -1,30 +1,11 @@
 import DOMParserReact from "dom-parser-react";
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <DOMParserReact
-          source="<!-- Google Tag Manager -->
-            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WDMQGFW');</script>
-            <!-- End Google Tag Manager -->"
-        />
-        <DOMParserReact
-          source={`<!-- Google tag (gtag.js) -->
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-3SLHT42S29"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-3SLHT42S29');
-          </script>`}
-        />
         <link
           rel="icon"
           href="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Android_O_Preview_Logo.png/1024px-Android_O_Preview_Logo.png"
@@ -50,6 +31,25 @@ export default function Document() {
         </noscript>
         <Main />
         <NextScript />
+        <Script strategy="beforeInteractive" id="gtag">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WDMQGFW');`}
+        </Script>
+        <Script
+          strategy="beforeInteractive"
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-3SLHT42S29"
+        ></Script>
+        <Script strategy="beforeInteractive" id="gtag-analyzer">
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag("js", new Date());
+
+          gtag("config", "G-3SLHT42S29");`}
+        </Script>
       </body>
     </Html>
   );
