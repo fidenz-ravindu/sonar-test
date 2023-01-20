8a11f2522a1de1db43d9a96838b0a32fa0afe981
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2023-01-21 00:44:55 +0530

    gtag anlz

--- pages/_document.js
+++ pages/_document.js
@@ -7,12 +7,23 @@ export default function Document() {
       <Head>
         <DOMParserReact
           source="<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WDMQGFW');</script>
-<!-- End Google Tag Manager -->"
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WDMQGFW');</script>
+            <!-- End Google Tag Manager -->"
+        />
+        <DOMParserReact
+          source={`<!-- Google tag (gtag.js) -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-3SLHT42S29"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-3SLHT42S29');
+          </script>`}
         />
         <link
           rel="icon"
