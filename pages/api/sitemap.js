c234cc9ec87c9148ee57d20aeaec4942469e97d4
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2023-02-02 11:53:51 +0530

    response changed

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,5 +1,5 @@
 import map from "../../public/sitemap.xml";
 
 export default async function handler(req, res) {
-  res.status(200).send(map);
+  res.status(200).send(map.urlset.url.map((elm) => elm.loc).join("\n"));
 }
